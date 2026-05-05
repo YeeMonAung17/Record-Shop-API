ebf7be8f79c6709c08ea1775306dd8c789b166a1
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-05-05 10:35:49 +0100

    feat: create Album model

---
+++ Models/Album.cs
@@ -0,0 +1,13 @@
+﻿namespace Record_Shop.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
