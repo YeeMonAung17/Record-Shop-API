c4fd686eb2e531404bcc8e36a00d4dcec0d643c8
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-05-05 10:35:31 +0100

    feat: add RecordShop DbContext

---
+++ Data/RecordDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Record_Shop.Models;
+
+namespace Record_Shop.Data
+{
+    public class RecordDbContext : DbContext
+    {
+
+        public DbSet<Album> Albums { get; set; }
+
+        public RecordDbContext(DbContextOptions<RecordDbContext> options) : base(options) { }
+        
+    }
+}
